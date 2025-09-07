e55f67a44da9a32c403fb88be59d4e09dad41079
MateoGiuffra <matteogiuffrah40@gmail.com>
2025-09-07 18:34:05 -0300

    subo server tcp para ver h2 en intellij

--- src/main/java/com/desapp/football_api/FootballApiApplication.java
+++ src/main/java/com/desapp/football_api/FootballApiApplication.java
@@ -1,13 +1,22 @@
 package com.desapp.football_api;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class FootballApiApplication {
 
 	public static void main(String[] args) {
+		try {
+			Server tcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifNotExists").start();
+			System.out.println("H2 TCP server started at port: " + tcpServer.getPort());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		SpringApplication.run(FootballApiApplication.class, args);
 	}
-
 }
