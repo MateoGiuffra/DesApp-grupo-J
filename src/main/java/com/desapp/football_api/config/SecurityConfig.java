82a927ceea1685233a72c439330d9c7e8d719a2f
MateoGiuffra <matteogiuffrah40@gmail.com>
2025-09-10 23:39:19 -0300

    delete innecesaries imports

--- src/main/java/com/desapp/football_api/config/SecurityConfig.java
+++ src/main/java/com/desapp/football_api/config/SecurityConfig.java
@@ -2,25 +2,13 @@ package com.desapp.football_api.config;
 
 import com.desapp.football_api.security.JwtUtil;
 import com.desapp.football_api.service.UserService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.Optional;
 
 @Configuration
 public class SecurityConfig {
