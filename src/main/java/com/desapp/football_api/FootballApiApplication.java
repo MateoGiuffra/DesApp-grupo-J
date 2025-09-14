be76cea6885c8dcc05b5e6df260c90c68af06a0d
MateoGiuffra <matteogiuffrah40@gmail.com>
2025-09-14 15:36:26 -0300

    new public endpoint added in security config

--- src/main/java/com/desapp/football_api/FootballApiApplication.java
+++ src/main/java/com/desapp/football_api/FootballApiApplication.java
@@ -11,7 +11,7 @@ public class FootballApiApplication {
 
 	public static void main(String[] args) {
 		try {
-			Server tcpServer = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifNotExists").start();
+			Server.createTcpServer("-tcp", "-tcAllowOthers", "-ifNotExists").start();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
